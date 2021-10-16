68ad948578fd0d5439b90f887d4851641bf0f43d
Karna Sjoberg <kesjoberg@gmail.com>
2021-10-16 14:33:23 -0500

    Added teamBuilder.js and updated code to link all the classes and subclasses

--- index.js
+++ index.js
@@ -1,54 +1,118 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const renderTeam = require('./src/teamBuilder')
 
-const employee = new Employee;
+const team =[];
 
-function initQuestion(){
+
+const initQuestions = () =>{
   inquirer
     .prompt(
     {
       type: 'input',
-      message: "Welcome to the Team Profile Generator! Are you ready to start entering the data for your Team Manager?",
-      name: 'uread',
-    },)
-}
-
-function askQuestions(){
-  inquirer
-    .prompt(
-    {
-      type: 'input',
-      message: "What is this team member's name?",
+      message: "Welcome to the Team Profile Generator! What is your team manager's name?",
       name: 'name',
     },
     {
       type: 'input',
-      message:   "What is the employee's ID number?",
+      message:   "What is the team managers's ID number?",
       name: 'id',
     },
     {
       type: 'input',
-      message:  "What is the employee's E-mail?",
+      message:   "What is the team manager's e-mail?",
       name: 'email',
     },
     {
       type: 'input',
-      message:  "What is the employee's office number?",
+      message:  "What is the team manager's office number?",
       name: 'officeNumber',
     })
-    .then(response =>
-      askMoreTeamMembers())
+    .then(response => {
+      const manager = new Manager(response.name, response.endQuestions, response.email, response.officeNumber)
+      team.push(manager);
+      askMoreTeamMembers()
+      
+    })
+   
+    
+}
+const engineerQuestions = () =>{
+  inquirer
+    .prompt(
+    {
+      type: 'input',
+      message: " What is the engineer's name?",
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message:   "What is the engineer's ID number?",
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message:   "What is the engineer's e-mail?",
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message:  "What is the engineer's GitHub username?",
+      name: 'github',
+    })
+    .then(response => {
+      const engineer = new Engineer(response.name, response.endQuestions, response.email, response.github)
+      team.push(engineer);
+      askMoreTeamMembers()
+    })
+    
+    
+}
+const internQuestions = () =>{
+  inquirer
+    .prompt(
+    {
+      type: 'input',
+      message: " What is the intern's name?",
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message:   "What is the intern's ID number?",
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message:   "What is the intern's e-mail?",
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message:  "What school does the intern attend?",
+      name: 'school',
+    })
+    .then(response => {
+      const intern = new Intern(response.name, response.endQuestions, response.email, response.school)
+      team.push(intern);
+      askMoreTeamMembers()
+    })
+    
+
+    
 }
 
   
-function askMoreTeamMembers(){
+const askMoreTeamMembers = ()=> {
   inquirer
     .prompt(
       {
         type: 'list',
         message:  "Select the next team member you would like to complete or select 'Finish' if you are done building your team.",
         choices: [ 'Engineer', 'Intern', 'Finished'],
-        name: 'nextQuestion',
+        name: 'role',
       })
      .then(response => {
       if(response === 'Engineer'){
@@ -61,30 +125,14 @@ function askMoreTeamMembers(){
      })
 }
 
-function engineerQuestions() {
-  inquirer
-    .prompt(
-      {
-        type: 'input',
-        message: "What is the engineer's GitHub user name",
-        name: 'github',
-      },
-    )
-    .then(response =>
-      askMoreTeamMembers())
-} 
-function internQuestions() {
-  inquirer
-    .prompt(
-      {
-        type: 'input',
-        message: "What school does the intern attend?",
-        name: 'school',
-      },
-    )
-    .then(response =>
-      askMoreTeamMembers())
-} 
 
-askQuestions();
+const endQuestions =() =>{
+  buildTeam();
+  console.log('Thank you for building your team, please go to the team.html to view your team.')
+}
+
+const buildTeam =() =>{
+  fs.writeFile('team.html', renderTeam(team), 'utf-8')
+}
+initQuestions();
 
---
+++ src/teamBuilder.js
@@ -0,0 +1,84 @@
+
+
+const renderTeam = (team) =>{ 
+
+
+  const generateManager = (manager) =>{
+    return `<div class="card" style="width: 18rem;">
+      <div class="card-header text-white bg-primary mb-3">
+        ${manager.getName()} <br>
+        <i class="fas fa-mug-hot"></i>Manager
+      </div>  
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.getId()}</li>
+        <li class="list-group-item">Email: ${manager.getEmail()}</li>
+        <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+      </ul>
+    </div>`
+  }
+  const generateEngineer = (engineer) =>{
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-header text-white bg-primary mb-3">
+      ${engineer.getName()} <br>
+      <i class="fas fa-glasses"></i> Engineer
+    </div>
+  
+    
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+        <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+      </ul>
+    </div>`
+  }
+
+  const generateIntern =(intern) =>{
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-header text-white bg-primary mb-3">
+      ${intern.getName()} <br>
+      <i class="fas fa-user-graduate"></i>Intern
+    </div>
+  
+    
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${intern.getId()}</li>
+      <li class="list-group-item">Email: ${intern.getEmail()}</li>
+      <li class="list-group-item">School: ${intern.getSchool()}</li>
+    </ul>
+  </div>`
+  }
+
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+    crossorigin="anonymous"
+  />
+    <title>Document</title>
+  </head>
+  <header>
+    <div class="jumbotron text-white bg-danger">
+      <h1 class="display-4">My Team!</h1>
+    </div>
+  
+  </header>
+  <body> 
+    ${managerHtml}
+    ${engineerHtml}
+    ${internHtml}
+  </body>
+  </html>`
+
+  const engineerHtml = team.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)).join('')
+  const internHtml = team.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)).join('')
+  const managerHtml = team.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager))
+  console.log(team);
+}
+module.exports = renderTeam    
